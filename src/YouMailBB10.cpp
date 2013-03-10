58d300958b30e269038a2371cd405140faae1631
Jacob Penny <jacob.penny@gmail.com>
2013-03-10 19:54:41 -0400

    a

--- src/YouMailBB10.cpp
+++ src/YouMailBB10.cpp
@@ -1,4 +1,3 @@
-// Tabbed pane project template
 #include "YouMailBB10.hpp"
 #include "api/method/apimethod.h"
 #include "api/method/apimethodresponsehandler.h"
@@ -10,31 +9,23 @@
 
 #include <QtDebug>
 
-
-
 using ymbb10::api::method::Authenticate;
-//using ymbb10::api::method::ResponseMessage;
 using ymbb10::api::method::ApiMethodBase;
 using ymbb10::api::method::ApiMethodResponseHandler;
 
-
 using namespace bb::cascades;
 using namespace bb::system;
-
-
 
 YouMailBB10::YouMailBB10(bb::cascades::Application *app) : QObject(app)
 {
 	QmlDocument *qml = QmlDocument::create("asset:///main.qml").parent(this);
 	AbstractPane *root = qml->createRootObject<AbstractPane>();
 
-
 	apiClient_ = new ymbb10::api::ApiClient("http://api.youmail.com/api", "youmailapp", app, this);
 
 	ApiMethodResponseHandler* responseHandler = new ApiMethodResponseHandler; // should this be heap?
 
-	// declare our enum so it works with slots
-
+	// Declare our enum so it works with slots
     qRegisterMetaType<ymbb10::api::method::ResponseMessage>("ymbb10::api::method::ResponseMessage");
 
 	bool r1 = QObject::connect(responseHandler, SIGNAL(responseProcessed(ymbb10::api::method::ResponseMessage)),
@@ -42,7 +33,6 @@ YouMailBB10::YouMailBB10(bb::cascades::Application *app) : QObject(app)
 
 	bool r2 = QObject::connect(apiClient_, SIGNAL(responseDeserialized(ymbb10::api::method::ApiMethodBase*)),
 			responseHandler, SLOT(handleResponse(ymbb10::api::method::ApiMethodBase*)));
-
 
 	Q_ASSERT(r1);
 	Q_ASSERT(r2);
@@ -67,8 +57,6 @@ YouMailBB10::YouMailBB10(bb::cascades::Application *app) : QObject(app)
 	app->setScene(root);
 }
 
-
-
 void YouMailBB10::handleLoginButtonClicked()
 {
 	QObject *userPhoneField = loginSheet_->findChild<QObject*>("phone_number");
@@ -77,21 +65,8 @@ void YouMailBB10::handleLoginButtonClicked()
 	QObject *userPinField = loginSheet_->findChild<QObject*>("user_pin");
 	QVariant userPin = userPinField->property("text");
 
-	// Execute authenicate call here
-
-
 	QSharedPointer<ApiMethodBase> authCall(new Authenticate(userPhone.toString(), userPin.toString()));
 	apiClient_->execute(authCall);
-	/*
-	bool authSucessful = (userPin.toString() == userPhone.toString()); // place holder
-	if (authSucessful) {
-		// store auth token
-		loginSheet_->close();
-	} else {
-		//showAuthFailedToast();
-		 *
-	}
-	*/
 }
 
 void YouMailBB10::responseMessage(ymbb10::api::method::ResponseMessage message) {
@@ -100,8 +75,6 @@ void YouMailBB10::responseMessage(ymbb10::api::method::ResponseMessage message) 
 		loginSheet_->close();
 	}
 }
-
-
 
 void YouMailBB10::showAuthFailedToast() {
     SystemToast *toast = new SystemToast(this); // leak?
